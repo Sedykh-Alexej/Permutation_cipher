01175631ae1de589a0a0e8dec8dc51f3be6105ed
Sedykh-Alexej <sedx.alexej.4948@mail.ru>
2023-06-11 12:39:44 +0300

    first commit

---
+++ Laba1/LabePage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Laba1
+{
+    /// <summary>
+    /// Логика взаимодействия для LabePage.xaml
+    /// </summary>
+    public partial class LabePage : Page
+    {
+        Transposition t;
+
+        public LabePage()
+        {
+            InitializeComponent();
+            t = new Transposition();
+        }
+
+        private void Выход(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Start(object sender, RoutedEventArgs e)
+        {
+            t.SetKey(KeyTextBox.Text);
+
+            if (EncryptRadioButton.IsPressed)
+                OutputTextBox.Text = t.Encrypt(InputTextBox.Text);
+            else
+                OutputTextBox.Text = t.Decrypt(InputTextBox.Text);
+        }
+    }
+}
---
+++ Laba1/Transposition.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba1
+{
+    class Transposition
+    {
+        private int[] key = null;
+
+        public void SetKey(int[] _key)
+        {
+            key = new int[_key.Length];
+            for (int i= 0; i<_key.Length; i++)
+            {
+                key[i] = _key[i];
+            }
+        }
+
+        public void SetKey(string[] _key)
+        {
+            key = new int[_key.Length];
+            for (int i = 0; i < _key.Length; i++)
+            {
+                key[i] = Convert.ToInt32(_key[i]);
+            }
+        }
+
+        public void SetKey(string _key)
+        {
+            string[] keyMass = _key.Split(' ');
+
+            SetKey(keyMass);
+        }
+
+        public string Encrypt (string input)
+        {
+            for (int i = 0; i < input.Length % key.Length; i++)
+                input += input[i];
+
+            string result = "";
+
+            for(int i=0; i<input.Length; i+=key.Length)
+            {
+                char[] transposition = new char[key.Length];
+
+                for (int j = 0; j < key.Length; j++)
+                    transposition[key[j] - 1] = input[i + j];
+
+                for (int j = 0; j < key.Length; j++)
+                    result += transposition[j];
+            }
+            return result;
+        }
+
+        public string Decrypt(string input)
+        {
+            string result = "";
+
+            for (int i = 0; i < input.Length; i += key.Length)
+            {
+                char[] transposition = new char[key.Length];
+
+                for (int j = 0; j < key.Length; j++)
+                    transposition[j] = input[i + key[j]-1];
+
+                for (int j = 0; j < key.Length; j++)
+                    result += transposition[j];
+            }
+            return result;
+        }
+    }
+}
